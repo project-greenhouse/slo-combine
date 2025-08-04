ee6003415ec300dd202cc478c63f01c5f8b0107c
laureng-hd <lauren@hawkindynamics.com>
2025-08-03 22:06:30 -0700

    update for render.com deployment

--- test.py
+++
@@ -1,6 +0,0 @@
-import plotly.graph_objects as go
-fig = go.Figure(data=go.Bar(y=[2, 3, 1]))
-fig.to_image(format="png")  # This should now succeed
-
-# 
-print("Plotly image conversion works correctly.")
